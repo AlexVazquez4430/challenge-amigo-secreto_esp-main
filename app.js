a2fc511059cdb2c7d19467c17431a07dc1ae49b4
AlexVazquez4430 <161274923+AlexVazquez4430@users.noreply.github.com>
2025-09-06 00:13:00 -0600

    insertDatos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables que tomo del HTML
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = document.getElementById("listaAmigos");
+
+// Función para agregar un amigo
+function agregarAmigo() {
+  nombre = inputAmigo.value;
+  if (nombre === "") {
+    alert("Favor de ingresar un nombre válido");
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.textContent = nombre;
+  listaAmigos.appendChild(li);
+
+  inputAmigo.value = "";
+}
